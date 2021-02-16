b3f6f61fe998685d39243bb76a0f9df2159ceb4e
Emily <emnagai89@gmail.com>
2021-02-15 19:17:31 -0800

    secrets

---
+++ secrets_file.py
@@ -0,0 +1,57 @@
+from dotenv import load_dotenv
+import os
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '=&(oeq4bslb0ii%9icq%c*6utibd0na9h6$h45f528x=n#=68p'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+# Database
+# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mindfull',
+        'USER': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432'
+    }
+}
+
+# Password validation
+# https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+# SOCIALACCOUNT_PROVIDERS = {
+#     'google': {
+#         # For each OAuth based provider, either add a ``SocialApp``
+#         # (``socialaccount`` app) containing the required client
+#         # credentials, or list them here:
+#         'APP': {
+#             'client_id': '123',
+#             'secret': '456',
+#             'key': ''
+#         }
+#     }
+
+# def printenvironment():
+#     print(f'The client id is: TEST.')
+
+# if __name__ == "__main__":
+#     printenvironment()
