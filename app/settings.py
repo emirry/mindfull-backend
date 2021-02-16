43c3e544f442b27f7735693ce44d346f0a1f19b2
Emily <emnagai89@gmail.com>
2021-02-15 20:29:02 -0800

    settings

--- app/settings.py
+++ app/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 # from dotenv import load_dotenv
-# import os
+import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
