eea4c1d7a900fc744788a19f0d949d7085b33f1d
Emily <emnagai89@gmail.com>
2021-02-15 20:35:14 -0800

    typo

--- app/settings.py
+++ app/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-from secrets_file.py import *
+from secrets_file import *
 # from dotenv import load_dotenv
 import os
 
